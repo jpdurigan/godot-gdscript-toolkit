a0ca459769e7d354ea17aad342a2251ae49fa655
jp durigan <jpdurigan@gmail.com>
2025-09-07 16:44:42 -0300

    adding ignore tag test case

---
+++ tests/formatter/input-output-pairs/custom-ignore-tag.in.gd
@@ -0,0 +1,26 @@
+func a():
+	var b      # breaking comments conventions
+	var ccccc  # breaking comments conventions
+
+	var test = c(
+		b,
+		ccccc,
+		b
+	)
+
+
+# gdformat: off
+func b():
+	var b      # breaking comments conventions
+	var ccccc  # breaking comments conventions
+
+	var test = c(
+		b,
+		ccccc,
+		b
+	)
+# gdformat: on
+
+
+func c(a, b, c):
+	pass
